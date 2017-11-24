bec0c8ca757035c8ed17df9ccd3c9b746285f682
ziaochina <ziaochina@gmail.com>
2017-11-24 10:09:10 +0800

    zlj

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import appMiddleware from './appMiddleware'
 import reducer from './reducer'
 import config from './config'
 import start from './start'
-import {registerApp,registerApps} from './appFactory'
+import appFactory from './appFactory'
 import init from './init'
 
+const {registerApp, registerApps} = appFactory
 export {
 	AppLoader,
 	appMiddleware,
